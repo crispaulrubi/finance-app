4298aa97e8fd9e2f5541929d385507fb6e24bb59
crsplrb <crispauljohn.rubi@gmail.com>
2024-02-04 18:51:00 +0800

    feat: google sso client id

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { SidebarProvider } from 'src/contexts/SidebarContext';
 import * as serviceWorker from 'src/serviceWorker';
 
 ReactDOM.render(
-  <GoogleOAuthProvider clientId="<your_client_id>">
+  <GoogleOAuthProvider clientId="680003898888-efcjh347etdavf018bg0a8ijvobpb0tp.apps.googleusercontent.com">
     <HelmetProvider>
       <SidebarProvider>
         <BrowserRouter>
